92bc5036d875a7782ce7b3398e547b5ecc1afbd5
David Portella <david@davidportella.cl>
2015-08-11 20:43:33 +0000

    The module "config" is added to give more flexibility to the configuration settings.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var restify = require("restify");
+var restify = require("restify"),
+    config  = require("config")
+    ;
 
 var server = restify.createServer();
 
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.listen(9008, function () {
+server.listen(config.get('port'), function () {
     console.log("Basic server started successfully...");
     console.log('%s listening at %s', server.name, server.url);
 });
