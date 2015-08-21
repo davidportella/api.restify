8d92bab422d432ac901a56b71e8e91c799d7b984
David Portella <david@davidportella.cl>
2015-08-21 15:41:36 -0300

    Soporte de versiones en el modulo dummy por medio de cabecera "accept-version"

--- api/dummy.js
+++ api/dummy.js
@@ -1,11 +1,26 @@
+var base_path = '/dummy/';
+
+function dummyGet(req, res, next) {
+    res.send({dummy: true});
+    return next();
+}
+
+var send_version = ':name';
+
+function sendV1(req, res, next) {
+    res.send('hello: ' + req.params.name);
+    return next();
+}
+
+function sendV2(req, res, next) {
+    res.send({hello: req.params.name});
+    return next();
+}
+
 var dummy = function (server) {
-
-    server.get('/dummy/get/', function (req, res, next) {
-
-        res.send({dummy: true});
-
-    });
-
+    server.get({path: base_path + 'get'}, dummyGet);
+    server.get({path: base_path + send_version, version: '1.1.3'}, sendV1);
+    server.get({path: base_path + send_version, version: '2.0.0'}, sendV2);
 };
 
 module.exports = dummy;
